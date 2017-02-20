ba19739747d29f07ec91bbdc2df271726ec0068e
Daniel Pavone <danielpavone@gmail.com>
2017-02-20 04:20:05 +0100

    Adding new route auth

--- src/components/user/routes.js
+++ src/components/user/routes.js
@@ -1,6 +1,7 @@
 (function(){
-    var database = require('../../settings/config').database;
-    var Knex = require('knex')(database);
+    var config = require('../../settings/config');
+    var Knex = require('knex')(config.database);
+    var jwt = require('jsonwebtoken');
     
     module.exports = [{
         path: '/users',
@@ -10,7 +11,7 @@
                 if(!results || results.length === 0) {
                     reply({
                         error: true,
-                        errMessage: 'No users'
+                        errMessage: 'Users not found'
                     });
                 }
 
@@ -18,6 +19,42 @@
                     dataCount: results.length,
                     data: results,
                 });
+            }).catch((err) => {
+                reply('server-side error');
+            });
+        }
+    },{
+        path: '/auth',
+        method: 'POST',
+        handler: (request, reply) => {
+            const { username, password } = request.payload;
+
+            const getOperation = Knex('users').where({
+                username
+            }).select('guid', 'password').then(([user]) => {
+                if(!user) {
+                    reply({
+                        error: true,
+                        errMessage: 'the specified user was not found'
+                    });
+                    return;
+                }
+                if(user.password === password) {
+                    const token = jwt.sign({
+                        username,
+                        scope: user.guid
+                    }, config.jwt.key ,{
+                        algorithm: 'HS256',
+                        expiresIn: '1h'
+                    });
+
+                    reply({
+                        token,
+                        scope: user.guid
+                    });
+                } else {
+                    reply('Incorrect password');
+                }
             }).catch((err) => {
                 reply('server-side error');
             });
--- src/utils/seeds/00_Users.js
+++ src/utils/seeds/00_Users.js
@@ -1,23 +1,23 @@
 exports.seed = function (knex, Promise) {
-  // Deletes ALL existing entries
+	// Deletes ALL existing entries
 	return knex('users').del()
-    	.then(function () {
-      	// Inserts seed entries
-      	return knex('users').insert([
-        	{
-        		name: 'User One',
-        		username: 'userone',
-				password: '123456',
-				email: 'userone@example.com',
-				guid: 'f03ede7c-b121-4112-bcc7-130a3e87988c'
-      		},
-			{
-        		name: 'User Two',
-        		username: 'usertwo',
-				password: '123456',
-				email: 'usertwo@example.com',
-				guid: 'f93gdy7c-b051-5302-bdb5-400n3e62044t'
-      		}
-		]);
-    });
+		.then(function () {
+			// Inserts seed entries
+			return knex('users').insert([
+				{
+					name: 'User One',
+					username: 'userone',
+					password: '123456',
+					email: 'userone@example.com',
+					guid: 'f03ede7c-b121-4112-bcc7-130a3e87988c'
+				},
+				{
+					name: 'User Two',
+					username: 'usertwo',
+					password: '123456',
+					email: 'usertwo@example.com',
+					guid: 'f93gdy7c-b051-5302-bdb5-400n3e62044t'
+				}
+			]);
+		});
 };
