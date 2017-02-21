c6cff2f7839f1403ebda57dcc72a83063e720417
Daniel Pavone <danielpavone@gmail.com>
2017-02-21 01:29:59 +0100

    Adding put, delete methods

--- src/components/player/routes.js
+++ src/components/player/routes.js
@@ -20,5 +20,69 @@
             }
         },
         handler: controller.post
+    }, {
+        path: '/players/{id}',
+        method: 'PUT',
+        config: {
+            auth: {
+                strategy: 'token',
+                scope: ['admin']
+            },
+            pre: [{
+                method: (request, reply) => {
+                    const {
+                        id
+                    } = request.params;
+                    const {
+                        scope
+                    } = request.auth.credentials;
+
+                    const getOperation = Knex('players').where({
+                        id: id,
+                    }).then(([result]) => {
+                        if (!result) {
+                            reply({
+                                error: true,
+                                errMessage: 'the player with id ${id} was not found'
+                            }).takeover();
+                        }
+                        return reply.continue();
+                    });
+                }
+            }],
+            handler: controller.put
+        }
+    }, {
+        path: '/players/{id}',
+        method: 'DELETE',
+        config: {
+            auth: {
+                strategy: 'token',
+                scope: ['admin']
+            },
+            pre: [{
+                method: (request, reply) => {
+                    const {
+                        id
+                    } = request.params;
+                    const {
+                        scope
+                    } = request.auth.credentials;
+
+                    const getOperation = Knex('players').where({
+                        id: id,
+                    }).then(([result]) => {
+                        if (!result) {
+                            reply({
+                                error: true,
+                                errMessage: 'the player with id ${id} was not found'
+                            }).takeover();
+                        }
+                        return reply.continue();
+                    });
+                }
+            }],
+            handler: controller.del
+        }
     }];
 })();
