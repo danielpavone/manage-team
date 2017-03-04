0eb4e4d0c9cbec91570d87da3fb33389e02a9d06
Daniel Pavone <danielpavone@gmail.com>
2017-03-04 20:07:09 +0100

    Style guide

--- tasks/clean.js
+++ tasks/clean.js
@@ -2,9 +2,9 @@ module.exports = function (grunt) {
 	'use strict';
 
 	grunt.config('clean', {
-	    build: [
-            'app/'
-        ]
+		build: [
+			'app/'
+		]
 	});
 	grunt.loadNpmTasks('grunt-contrib-clean');
 };
