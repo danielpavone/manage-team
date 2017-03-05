3cad8340477339e4581979b30a4d10f0b677313c
Daniel Pavone <danielpavone@gmail.com>
2017-03-05 02:42:30 +0100

    More tests were added

--- test/user.js
+++ test/user.js
@@ -10,7 +10,7 @@
 
     describe('/POST user', () => {
 
-        it('POST /auth (endpoint test)', (done) => {
+        it('POST /auth should be BadRequest', (done) => {
             let options = {
                 method: 'POST',
                 url: '/auth'
@@ -22,7 +22,7 @@
             });
         });
 
-        it('POST /auth failed', (done) => {
+        it('POST /auth should fail', (done) => {
             let options = {
                 method: 'POST',
                 url: '/auth',
@@ -34,11 +34,15 @@
 
             server.inject(options, (response) => {
                 Code.expect(response.statusCode).to.equal(400);
+                Code.expect(response.result).to.be.an.object();
+                Code.expect(response.result).to.have.length(3);
+                Code.expect(response.result.error).to.equal('Bad Request');
+                Code.expect(response.result.message).to.equal('The specified user was not found');
                 server.stop(done);
             });
         });
 
-        it('POST /auth login', (done) => {
+        it('POST /auth should login', (done) => {
             let options = {
                 method: 'POST',
                 url: '/auth',
@@ -51,7 +55,10 @@
             server.inject(options, (response) => {
                 Code.expect(response.statusCode).to.equal(200);
                 Code.expect(response.result).to.be.an.object();
+                Code.expect(response.result.admin).to.exist();
+                Code.expect(response.result.token).to.exist();
                 Code.expect(response.result.admin).to.be.a.number();
+                Code.expect(response.result.token).to.be.a.string();
                 Code.expect(response.result).to.have.length(2);
                 server.stop(done);
             });
@@ -68,6 +75,7 @@
             server.inject(options, (response) => {
                 Code.expect(response.statusCode).to.equal(200);
                 Code.expect(response.result).to.have.length(9);
+                Code.expect(response.result).to.be.an.string();
                 server.stop(done);
             });
         });
